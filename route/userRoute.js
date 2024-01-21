a8eb9724eee0e33646e720b4061ad8ab9f3c1f74
Agboola Akorede <akoredeagboola23@gmail.com>
2024-01-21 02:43:42 +0100

    initial commit

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 const { myPortfolio, contactPost} = require('../controller/userController');
 
 router.get('/', myPortfolio);
